225bab4db9f8e4f2786beed20d5930901f75b991
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-04-01 10:54:56 +0200

    First version of doc

--- PDF.pm
+++ PDF.pm
@@ -55,3 +55,158 @@ sub format_datetime {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+DateTime::Format::PDF - PDF DateTime Parser and Formatter.
+
+=head1 SYNOPSIS
+
+ use DateTime::Format::PDF;
+
+ my $obj = DateTime::Format::PDF->new;
+ my $dt = $obj->parse_datetime($pdf_date);
+ my $pdf_date = $obj->format_datetime($dt);
+
+=head1 DESCRIPTION
+
+This module understands the formats used by PDF file. It can be used to parse these formats in order
+to create L<DateTime> objects, and it can take a DateTime object and produce a string representing
+it in a format accepted by PDF.
+
+=head1 METHODS
+
+=head2 C<new>
+
+ my $obj = DateTime::Format::PDF->new(%params);
+
+Constructor.
+
+Returns instance of object.
+
+=head2 C<parse_datetime>
+
+ my $dt = $obj->parse_datetime($pdf_date);
+
+Parse PDF datetime string.
+
+Possible valid strings:
+
+=over
+
+=item C<D:YYYY>
+
+=item C<D:YYYYMM>
+
+=item C<D:YYYYMMDD>
+
+=item C<D:YYYYMMDDHH>
+
+=item C<D:YYYYMMDDHHmm>
+
+=item C<D:YYYYMMDDHHmmSS>
+
+=item C<D:YYYYMMDDHHmmSSZ>
+
+=item C<D:YYYYMMDDHHmmSSOHHmm>
+
+=item C<D:YYYYMMDDHHmmSSOHH'mm>
+
+=item C<D:YYYYMMDDHHmmSSOHH'mm'>
+
+=item C<D:YYYYMMDDHHmmSSOHHmm'>
+
+=back
+
+Returns L<DateTime> object.
+
+=head2 C<format_datetime>
+
+ my $pdf_date = $obj->format_datetime($dt);
+
+Format L<DateTime> object to PDF datetime string.
+Output value is C<D:YYYYMMDDHHmmSSOHHmm>.
+
+Returns string.
+
+=head1 ERRORS
+
+TODO
+
+=head1 EXAMPLE1
+
+=for comment filename=parse_pdf_string.pl
+
+ use strict;
+ use warnings;
+
+ use DateTime::Format::PDF;
+
+ # Object.
+ my $obj = DateTime::Format::PDF->new;
+
+ # Parse date.
+ my $dt = $obj->parse_datetime("D:20240401084337-01'30");
+
+ # Print out.
+ print $dt->strftime("%a, %d %b %Y %H:%M:%S %z")."\n";
+
+ # Output like:
+ # Mon, 01 Apr 2024 08:43:37 -0130
+
+=head1 EXAMPLE2
+
+=for comment filename=format_pdf_string.pl
+
+ use strict;
+ use warnings;
+
+ use DateTime;
+ use DateTime::Format::PDF;
+
+ # Object.
+ my $obj = DateTime::Format::PDF->new;
+
+ # Example date.
+ my $dt = DateTime->now;
+
+ # Format.
+ my $pdf_date = $obj->format_datetime($dt);
+
+ # Print out.
+ print "PDF date: $pdf_date\n";
+
+ # Output like:
+ # PDF date: D:20240401084337+0000
+
+=head1 DEPENDENCIES
+
+L<DateTime::Format::Builder>.
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/DateTime-Format-PDF>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© 2024 Michal Josef Špaček
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
